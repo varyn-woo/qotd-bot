0db31f60f7f8e86f7434b37862f32d2ae01bf3d2
Varyn Woo <varyn.woo@gmail.com>
2026-04-18 13:17:55 -0700

    initial bot

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 import 'dotenv/config';
 import express from 'express';
+import https from 'https';
 import {
-  ButtonStyleTypes,
   InteractionResponseFlags,
   InteractionResponseType,
   InteractionType,
   MessageComponentTypes,
   verifyKeyMiddleware,
 } from 'discord-interactions';
-import { getRandomEmoji, DiscordRequest } from './utils.js';
-import { getShuffledOptions, getResult } from './game.js';
+import { getRandomEmoji } from './utils.js';
+import { addQuestion, listQuestions, setChannel, getChannel, dequeueQuestion } from './store.js';
 
 // Create an express app
 const app = express();
 // Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
-// To keep track of our active games
-const activeGames = {};
+const QUESTION_SEND_INTERVAL = 1000 * 60 * 60 * 24; // 1 day
 
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
@@ -40,22 +39,55 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
   if (type === InteractionType.APPLICATION_COMMAND) {
     const { name } = data;
 
-    // "test" command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          flags: InteractionResponseFlags.IS_COMPONENTS_V2,
-          components: [
-            {
-              type: MessageComponentTypes.TEXT_DISPLAY,
-              // Fetches a random emoji to send from a helper function
-              content: `hello world ${getRandomEmoji()}`
-            }
-          ]
-        },
-      });
+    // "add" command (adds a question)
+    switch (name) {
+      case 'add':
+        // Send a message into the channel where command was triggered from
+        const question = data.options[0].value;
+        addQuestion(question);
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+            components: [
+              {
+                type: MessageComponentTypes.TEXT_DISPLAY,
+                // Fetches a random emoji to send from a helper function
+                content: `${getRandomEmoji()} Question added: \`${question}\``,
+              }
+            ]
+          },
+        });
+      case 'list':
+        const questions = listQuestions();
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+            components: [
+              {
+                type: MessageComponentTypes.TEXT_DISPLAY,
+                content: `${getRandomEmoji()} Here are the questions currently in the queue:\n${questions.join('\n')}`,
+              }
+            ]
+          }
+        });
+      case 'setchannel':
+        const channelId = data.options[0].value;
+        setChannel(channelId);
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+            components: [
+              {
+                type: MessageComponentTypes.TEXT_DISPLAY,
+                content: `${getRandomEmoji()} Channel set to <#${channelId}>!`,
+              }
+            ]
+          }
+        });
+      case 'setinterval':
     }
 
     console.error(`unknown command: ${name}`);
@@ -65,6 +97,58 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
   console.error('unknown interaction type', type);
   return res.status(400).json({ error: 'unknown interaction type' });
 });
+
+setInterval(() => {
+  const channelId = getChannel();
+
+  if (channelId) {
+    // Send a QOTD message to the channel
+    const question = dequeueQuestion();
+    if (question) {
+      const data = JSON.stringify({
+        embeds: [
+          {
+            title: `${getRandomEmoji()} 𝙌𝙪𝙚𝙨𝙩𝙞𝙤𝙣 𝙤𝙛 𝙩𝙝𝙚 𝙙𝙖𝙮`,
+            type: 'rich',
+            allowed_mentions: {
+              parse: ['roles']
+            },
+            description: `\`${question}\`\n<@&1495125973216002058>`,
+            footer: {
+              text: 'You can submit questions to the bot using the /add command.'
+            }
+          }
+        ]
+      });
+      const options = {
+        hostname: `discord.com`,
+        path: `/api/channels/${channelId}/messages`,
+        method: 'POST',
+        headers: {
+          'Authorization': `Bot ${process.env.DISCORD_TOKEN}`,
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(data)
+        }
+      };
+
+      const req = https.request(options, (res) => {
+        let body = '';
+
+        res.on('data', chunk => body += chunk);
+        res.on('end', () => {
+          console.log('Response:', body);
+        });
+      });
+
+      req.on('error', (error) => {
+        console.error('Failed to send QOTD message:', error);
+      });
+
+      req.write(data);
+      req.end();
+    }
+  }
+}, QUESTION_SEND_INTERVAL);
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
--- commands.js
+++ commands.js
@@ -1,49 +1,76 @@
 import 'dotenv/config';
-import { getRPSChoices } from './game.js';
-import { capitalize, InstallGlobalCommands } from './utils.js';
+import { InstallGlobalCommands } from './utils.js';
 
-// Get the game choices from game.js
-function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
-
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
-
-  return commandChoices;
-}
-
-// Simple test command
-const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
+// Add new question
+const ADD_COMMAND = {
+  name: 'add',
+  description: 'Add a new question',
   type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 1, 2],
-};
-
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
   options: [
     {
       type: 3,
-      name: 'object',
-      description: 'Pick your object',
+      name: 'question',
+      description: 'The question to add',
       required: true,
-      choices: createCommandChoices(),
-    },
-  ],
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 2],
+    }
+  ]
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+// List all questions
+const LIST_COMMAND = {
+  name: 'list',
+  description: 'List all questions in the queue',
+  options: [],
+  type: 1
+};
+
+// Set channel for where you want to send the messages
+const CHANNEL_COMMAND = {
+  name: 'setchannel',
+  description: 'Set the channel where QOTD questions are sent',
+  options: [
+    {
+      type: 7,
+      name: 'channel',
+      description: 'The channel to send QOTD questions to',
+      required: true
+    }
+  ]
+}
+
+const INTERVAL_COMMAND = {
+  name: 'setinterval',
+  description: 'Set the interval for sending QOTD questions',
+  options: [
+    {
+      type: 4,
+      name: 'number',
+      description: 'The number of time units',
+      required: true
+    },
+    {
+      type: 3,
+      name: 'unit',
+      description: 'The time unit (seconds, minutes, hours)',
+      required: true,
+      choices: [
+        {
+          name: 'minutes',
+          value: 'minutes'
+        },
+        {
+          name: 'hours',
+          value: 'hours'
+        },
+        {
+          name: 'days',
+          value: 'days'
+        }
+      ]
+    }
+  ]
+}
+
+const ALL_COMMANDS = [ADD_COMMAND, LIST_COMMAND, CHANNEL_COMMAND, INTERVAL_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
